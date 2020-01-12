28725d4436f540e7921d3fbfab7df6a1051a26f0
restarmilan <restarmilan@gmail.com>
2020-01-12 15:12:50 +0100

    add parameterized test case for simple input field validation

--- tests/simple_form_demo_tests.py
+++ tests/simple_form_demo_tests.py
@@ -1,12 +1,22 @@
-from tests.base_test import BaseTest
+from ddt import file_data, ddt
+
 from simple_form_demo_page import SimpleFormDemoPage
+from tests.base_test import BaseTest
 
 
+@ddt
 class SimpleFormDemoTests(BaseTest):
 
-    def test_try_it(self):
+    @file_data('resources/simple_input_message.json')
+    def test_simple_input_field(self, message, expected):
         simple = SimpleFormDemoPage(self.driver)
         simple.navigate_to_simple_demo_form_page()
-        simple.set_user_input_to_simple_input_field("test-message")
+        simple.set_user_input_to_simple_input_field(message)
         simple.click_on_confirmation_button()
-        self.assertEqual(simple.get_user_message(), "test-message")
+        self.assertEqual(simple.get_user_message(), expected)
+
+
+if __name__ == "__main__":
+    # unittest.main()
+    tc = SimpleFormDemoTests()
+    super().run_single(tc, tc.test_simple_input_field)
