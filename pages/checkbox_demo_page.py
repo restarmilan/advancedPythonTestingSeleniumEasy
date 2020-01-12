7746c7c74ec2fce65a0921c45401063c4021d4ac
restarmilan <restarmilan@gmail.com>
2020-01-12 20:43:51 +0100

    add CheckboxDemoPage page subclass

---
+++ pages/checkbox_demo_page.py
@@ -0,0 +1,10 @@
+from base_page import BasePage
+
+
+class CheckboxDemoPage(BasePage):
+
+    def __init__(self, driver):
+        super().__init__()
+
+    def navigate_to_checkbox_demo_page:
+        super().navigate_to("basic-checkbox-demo.html")
