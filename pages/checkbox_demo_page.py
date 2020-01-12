83ddb5303ce8a802c1b54c4aa313f41598fa8462
restarmilan <restarmilan@gmail.com>
2020-01-12 21:41:47 +0100

    add locators, methods for clicking multiple checkbox

--- pages/checkbox_demo_page.py
+++ pages/checkbox_demo_page.py
@@ -1,10 +1,22 @@
+from selenium.webdriver.common.by import By
+
 from base_page import BasePage
 
 
 class CheckboxDemoPage(BasePage):
+    checkbox_options = (By.XPATH, "//input[@class='cb1-element']")
+    checkbox_button = (By.XPATH, "//input[@id='check1']")
 
     def __init__(self, driver):
-        super().__init__()
+        super().__init__(driver)
 
-    def navigate_to_checkbox_demo_page:
+    def navigate_to_checkbox_demo_page(self):
         super().navigate_to("basic-checkbox-demo.html")
+
+    def get_checkbox_button_value(self):
+        return self.driver.find_element(*self.checkbox_button).get_attribute('value')
+
+    def click_on_multiple_checkbox(self, inputs, value=1):
+        for i in range(len(inputs)):
+            if inputs[i] == value:
+                self.driver.find_elements(*self.checkbox_options)[i].click()
