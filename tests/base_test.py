2878ce0fd1b8aba2d653be2569f0fdaa3288bf7e
restarmilan <restarmilan@gmail.com>
2020-01-12 16:29:00 +0100

    add parameterized test case for double input field validation

--- tests/base_test.py
+++ tests/base_test.py
@@ -6,14 +6,8 @@ from selenium import webdriver
 class BaseTest(unittest.TestCase):
 
     def setUp(self):
-            self.driver = webdriver.Chrome("/home/rmilan/codecool/test_automation/chromedriver.exe")
-            self.driver.maximize_window()
+        self.driver = webdriver.Chrome("/home/rmilan/codecool/test_automation/chromedriver.exe")
+        self.driver.maximize_window()
 
     def tearDown(self):
-            self.driver.quit()
-
-    @staticmethod
-    def run_single(test_case, test_to_run):
-        test_case.setUp()
-        test_to_run()
-        test_case.tearDown()
+        self.driver.quit()
--- tests/simple_form_demo_tests.py
+++ tests/simple_form_demo_tests.py
@@ -1,3 +1,5 @@
+import unittest
+
 from ddt import file_data, ddt
 
 from simple_form_demo_page import SimpleFormDemoPage
@@ -7,16 +9,25 @@ from tests.base_test import BaseTest
 @ddt
 class SimpleFormDemoTests(BaseTest):
 
+    def setUp(self):
+        super().setUp()
+        self.simple = SimpleFormDemoPage(self.driver)
+        self.simple.navigate_to_simple_demo_form_page()
+
     @file_data('resources/simple_input_message.json')
     def test_simple_input_field(self, message, expected):
-        simple = SimpleFormDemoPage(self.driver)
-        simple.navigate_to_simple_demo_form_page()
+        simple = self.simple
         simple.set_user_input_to_simple_input_field(message)
         simple.click_on_confirmation_button()
         self.assertEqual(simple.get_user_message(), expected)
 
+    @file_data('resources/double_input_fields_data.json')
+    def test_double_input_fields(self, value1, value2, expected):
+        simple = self.simple
+        simple.set_double_user_input(value1, value2)
+        simple.add_values()
+        self.assertEqual(simple.get_added_values(), expected)
+
 
 if __name__ == "__main__":
-    # unittest.main()
-    tc = SimpleFormDemoTests()
-    super().run_single(tc, tc.test_simple_input_field)
+    unittest.main()
