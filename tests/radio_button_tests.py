e66e7ee33db6d584b42cf95e419cf5d13802530b
restarmilan <restarmilan@gmail.com>
2020-01-13 00:19:27 +0100

    add test cases for group radio button validation

--- tests/radio_button_tests.py
+++ tests/radio_button_tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from ddt import ddt, unpack, data
+from ddt import ddt, unpack, data, file_data
 
 from radio_button_demo_page import RadioButtonDemoPage
 from tests.base_test import BaseTest
@@ -20,8 +20,15 @@ class RadioButtonDemoTests(BaseTest):
     def test_simple_radio_button_demo(self, value, expected):
         radio = self.radio
         radio.click_simple_radio_buttons(value)
-        radio.get_selected_value()
-        self.assertEqual(radio.get_confirmation_message(), expected)
+        radio.get_selected_value_single_radio_button()
+        self.assertEqual(radio.get_confirmation_message_single_radio_button(), expected)
+
+    @file_data('resources/group_radio_buttons_data.json')
+    def test_multiple_radio_button_demo(self, value1, value2, expected):
+        radio = self.radio
+        radio.click_multiple_radio_buttons(value1, value2)
+        radio.get_selected_values_multiple_radio_buttons()
+        self.assertEqual(radio.get_confirmation_message_multiple_radio_button(), expected)
 
 
 if __name__ == "__main__":
