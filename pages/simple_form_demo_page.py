c3403f34fd4560392fa0476415244d1cf058bb00
restarmilan <restarmilan@gmail.com>
2020-01-12 16:27:03 +0100

    add methods and locators for double input field

--- pages/simple_form_demo_page.py
+++ pages/simple_form_demo_page.py
@@ -1,12 +1,16 @@
-from base_page import BasePage
 from selenium.webdriver.common.by import By
+
+from base_page import BasePage
 
 
 class SimpleFormDemoPage(BasePage):
-
     text_input_field = (By.XPATH, "//input[@id='user-message']")
     show_message_btn = (By.XPATH, "//button[text()='Show Message']")
     message_field = (By.XPATH, "//span[@id='display']")
+    value_a_field = (By.XPATH, "//input[@id='sum1']")
+    value_b_field = (By.XPATH, "//input[@id='sum2']")
+    get_total_button = (By.XPATH, "//button[text()='Get Total']")
+    added_values = (By.XPATH, "//span[@id='displayvalue']")
 
     def __init__(self, driver):
         super().__init__(driver)
@@ -23,4 +27,12 @@ class SimpleFormDemoPage(BasePage):
     def get_user_message(self):
         return self.driver.find_element(*self.message_field).text
 
+    def set_double_user_input(self, value1, value2):
+        self.driver.find_element(*self.value_a_field).send_keys(value1)
+        self.driver.find_element(*self.value_b_field).send_keys(value2)
 
+    def add_values(self):
+        self.driver.find_element(*self.get_total_button).click()
+
+    def get_added_values(self):
+        return self.driver.find_element(*self.added_values).text
